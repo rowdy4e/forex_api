65224f81480acb2be787f3a75b107106123d5f15
Rowdy <Rowdycz@gmail.com>
2025-01-09 19:58:23 +0100

    added Impact

--- scrapers.js
+++ scrapers.js
@@ -12,9 +12,18 @@ const scrapeForexFactory = async () => {
         const row = $(`.calendar__row[data-event-id]`);
 
         row.each((index, element) => {
+            
+            const impactClass = $(element).find('td.calendar__cell.calendar__impact span').attr('class');
+
             arr.push({
                 'time': $(element).find(`td.calendar__cell.calendar__time`).text().trim(),
                 'currency': $(element).find(`td.calendar__cell.calendar__currency`).text().trim(),
+                'impact': {
+                    'icon icon--ff-impact-gra': 'Non-Economic',
+                    'icon icon--ff-impact-yel': 'Low',
+                    'icon icon--ff-impact-ora': 'Medium',
+                    'icon icon--ff-impact-red': 'High'
+                }[impactClass] || 'Unknown',
                 'event': $(element).find(`td.calendar__cell.calendar__event`).text().trim(),
                 'actual': $(element).find(`td.calendar__cell.calendar__actual`).text().trim(),
                 'forecast': $(element).find(`td.calendar__cell.calendar__forecast`).text().trim(),
